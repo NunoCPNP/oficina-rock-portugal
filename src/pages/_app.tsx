0d63230decd0817b0b13df4965e2dcf227e1113b
Nuno Pereira <hello@nunopereira.tech>
2021-07-26 12:50:28 +0100

    add redux store config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
+import { Provider } from 'react-redux'
 import type { AppProps } from "next/app";
+
+import { useStore } from '../store/store'
 
 import GlobalStyles from "../styles/GlobalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const store = useStore(pageProps.initialReduxState)
+
   return (
-    <>
+    <Provider store={store}>
       <Component {...pageProps} />
       <GlobalStyles />
-    </>
+    </Provider>
   );
 }
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>o Tribos !</div>
+    <div>Yo Tribos !</div>
   )
 }
 
