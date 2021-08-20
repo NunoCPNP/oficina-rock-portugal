8bd7cedb6a7080c1056f636a7d7aa934515dde1f
Nuno Pereira <hello@nunopereira.tech>
2021-08-20 15:16:23 +0100

    dynamic header on layout

--- src/Layout/Default.js
+++ src/Layout/Default.js
@@ -1,14 +1,13 @@
 import styled from "@emotion/styled";
 import dynamic from "next/dynamic";
 
-import Header from "@/modules/Header";
-
 const Main = styled.main`
   max-width: 130rem;
   margin: 0 auto;
   padding-top: 11rem;
 `;
 
+const DynamicHeader = dynamic(() => import("@/modules/Header"));
 const DynamicFooter = dynamic(() => import("@/modules/Footer"));
 const DynamicCartDrawer = dynamic(() => import("@/modules/CartDrawer"));
 const DynamicGDPR = dynamic(() => import("@/modules/GDPR"));
@@ -16,7 +15,7 @@ const DynamicGDPR = dynamic(() => import("@/modules/GDPR"));
 const DefaultLayout = ({ children }) => {
   return (
     <>
-      <Header />
+      <DynamicHeader />
       <Main>{children}</Main>
       <DynamicFooter />
       <DynamicCartDrawer />
