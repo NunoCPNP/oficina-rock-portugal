f6cfff9a387ec2cedb469cfc835fb3cefa85059c
Nuno <hello@nunopereira.tech>
2021-09-08 22:36:29 +0100

    checkout

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,8 +3,13 @@ import useTranslation from 'next-translate/useTranslation'
 import SEO from '@/components/SEO'
 import SectionTitle from '@/components/SectionTitle'
 
+import useProduct from '@/hooks/useProduct'
+
 const CheckOut = () => {
   const { t } = useTranslation('common')
+  const [state, dispatch] = useProduct()
+
+  console.log('-->', state)
 
   return (
     <>
