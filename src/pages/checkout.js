14e15f6bede1b2f8793627427dc11394424e5b64
Nuno Pereira <hello@nunopereira.tech>
2021-09-08 23:48:33 +0100

    checkout

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,19 +2,21 @@ import useTranslation from 'next-translate/useTranslation'
 
 import SEO from '@/components/SEO'
 import SectionTitle from '@/components/SectionTitle'
+import CheckOutItems from '@/modules/CheckOutItems'
 
 import useProduct from '@/hooks/useProduct'
+import useAuth from '@/hooks/useAuth'
 
 const CheckOut = () => {
   const { t } = useTranslation('common')
-  const [state, dispatch] = useProduct()
-
-  console.log('-->', state)
+  const [{ shoppingBag }, dispatch] = useProduct()
+  const { user } = useAuth()
 
   return (
     <>
       <SEO title="Oficina Rock Portugal" description="" />
       <SectionTitle title={t(`checkout`)} offset="4rem" />
+      <CheckOutItems />
     </>
   )
 }
