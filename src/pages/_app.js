232c0e8952dddec2ca7cf48be5a5bf3a7b369cea
Nuno Pereira <nuno.nunespereira@farfetch.com>
2022-03-03 23:54:56 +0000

    remove scripts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,53 +1,12 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
 import DefaultLayout from '../Layout/Default'
-
-import Maintenance from '@/components/Maintenance'
 
 import { AuthProvider } from '@/context/AuthContext'
 import { ProductProvider } from '@/context/ProductContext'
 import { SettingsProvider } from '@/context/SettingsContext'
 
-import { toBoolean } from '@/utils/toBoolean'
-
 import GlobalStyles from '@/styles/GlobalStyles'
 
-export function reportWebVitals({ id, name, label, value }) {
-  window.gtag('event', name, {
-    event_category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-    event_label: id, // id unique to current page load
-    non_interaction: true,
-  })
-}
-
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  const handleRouteChange = (url) => {
-    window.gtag('config', process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, {
-      page_path: url,
-    })
-  }
-
-  useEffect(() => {
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
-  if (toBoolean(process.env.NEXT_PUBLIC_MAINTENANCE)) {
-    return (
-      <>
-        <Maintenance />
-        <GlobalStyles />
-      </>
-    )
-  }
-
   return (
     <SettingsProvider>
       <AuthProvider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,28 +5,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-          {/* Global Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=optional"
             rel="stylesheet"
           />
         </Head>
