84d8d10a1372b0f8d95bd110b4f3a5a52e3895d2
Nuno Pereira <nuno.nunespereira@farfetch.com>
2022-03-04 00:00:14 +0000

    font fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&display=swap"
             rel="stylesheet"
           />
         </Head>
